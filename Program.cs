ee75a4370048c8bfff229a17f4dd3d1b8232ba4c
joao <joaopedroazevedo1980@gmail.com>
2026-04-09 19:57:34 -0300

    Telefone

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Telefone");
+string telefone = Console.ReadLine()!;
+
+telefone = telefone.Remove(3)!;
+telefone = telefone.Insert(3, "-PIZZA");
+
+
+
+Console.WriteLine(telefone);
